60544a857bc3452158d73ec336c6a87c9612f799
inrvignesh <inrvignesh81@gmail.com>
2020-12-31 15:02:25 +0530

    first commit

--- main_part/urls.py
+++ main_part/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 from sub_part import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    #path('',views.contact,name="contact"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
